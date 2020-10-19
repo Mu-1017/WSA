8dba9b7108cbcf0ae866a67e9eb0afd55b3956fc
Scarlett <33408016+Mu-1017@users.noreply.github.com>
2020-10-19 16:58:36 +0800

    Update Jobs.js

--- Jobs.js
+++ Jobs.js
@@ -2,3 +2,4 @@ var container = document.getElementById("job-container");
 container.style.width = "100px";
 container.style.height = "100px";
 container.style.backgroundColor = "red";
+console.log("test test");
